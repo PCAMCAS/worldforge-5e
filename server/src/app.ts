c2ef91ce0719cf5315c76869e4c462605bcfcc52
PCAMCAS <castiwb@gmail.com>
2026-04-27 21:23:38 +0200

    fix(cors): allow vercel domains and previews

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,12 +15,17 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
+    origin(origin, callback) {
+      if (
+        !origin ||
+        allowedOrigins.includes(origin) ||
+        origin.endsWith('.vercel.app')
+      ) {
         callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
+        return
       }
+
+      callback(new Error(`Not allowed by CORS: ${origin}`))
     },
   }),
 )
